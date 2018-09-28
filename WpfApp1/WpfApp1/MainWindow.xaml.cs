3bce182abd1da376137203e23c057afb14900b87
Bjorn van der Veen <bjorn@live.co.za>
2018-09-28 13:07:36 +0200

    A butt-load has been done. Set up error catching, the initial part of keyboard event handling, the displays, etc.

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Microsoft.VisualBasic;
 namespace WpfApp1
 {
     /// <summary>
@@ -20,9 +20,65 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Key theKey;
+        public enum Rotors  { I, II, III, IV, V }
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        public void Settings()
+        {
+            int[] allRotors = new int[3] { comboRotorone.SelectedIndex, comboRotortwo.SelectedIndex, comboRotorthree.SelectedIndex};           
+            var rotorList = new List<string>();
+            foreach(int i in allRotors)
+            {
+                if (i <= -1)
+                {
+                    MessageBox.Show("Make sure settings are correct!");
+                    return;
+                }
+                switch (i)
+                {
+                    case (int)(Rotors.I):
+                        rotorList.Add("I");
+                        break;
+                    case (int)(Rotors.II):
+                        rotorList.Add("II");
+                        break;
+                    case (int)(Rotors.III):
+                        rotorList.Add("III");
+                        break;
+                    case (int)(Rotors.IV):
+                        rotorList.Add("IV");
+                        break;
+                    case (int)(Rotors.V):
+                        rotorList.Add("V");
+                        break;
+                }
+            }
+            string rotorsOrder="";
+            foreach(string a in rotorList)
+            {
+                rotorsOrder += a+" ";
+            }
+            RotorSetup.Text = rotorsOrder.TrimEnd();
+
+            if (string.IsNullOrWhiteSpace(startR1.Text)|| string.IsNullOrWhiteSpace(startR2.Text)|| string.IsNullOrWhiteSpace(startR3.Text))
+            {
+                MessageBox.Show("Make sure starting letters are correct!");
+                return;
+            }
+            LetterSetup.Text = (startR1.Text + startR2.Text + startR3.Text).ToUpper();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+             
+            MessageBox.Show(Convert.ToString(e.Key));
+            Settings();
         }
 
 
